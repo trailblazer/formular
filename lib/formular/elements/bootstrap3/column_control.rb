338b6e9c47bfa2c1f6f8fe4a8bf8b8eca8516e27
Fran Worley <frances@safetytoolbox.co.uk>
2016-07-22 10:50:06 +0100

    enable renaming html contexts and add bs stacked column controls

--- lib/formular/elements/bootstrap3/column_control.rb
+++ lib/formular/elements/bootstrap3/column_control.rb
@@ -23,6 +23,22 @@ module Formular
         def stacked_column?
           options[:stacked_col_class]
         end
+
+        rename_html_context(:default, :no_cols)
+
+        html(:stacked_column) do |element|
+          element.builder.row {
+            element.builder.div(
+              class: element.options[:stacked_col_class],
+              content: element.to_html(context: :no_cols)
+            )
+          }
+        end
+
+        html(:default) do |element|
+          context = element.stacked_column? ? :stacked_column : :no_cols
+          element.to_html(context: context)
+        end
       end # module ColumnControl
     end # module Bootstrap3
   end # module Elements
