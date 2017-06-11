a3fcfad1360642734ecc2e0250e618d91e879133
Fran Worley <frances@safetytoolbox.co.uk>
2017-06-11 11:59:33 +0100

    ergh. New erase cleans up whitespace!

--- test/erb_test.rb
+++ test/erb_test.rb
@@ -23,23 +23,7 @@ class Erb < Minitest::Spec
 
     it do
       Comment::Erb::Show.new(model).().must_equal %(<div>New</div>
-<form action="/posts" method="post" accept-charset="utf-8"><input name="utf8" type="hidden" value="✓"/>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea placeholder="And your story..." rows="9" name="comment[body]" id="comment_body"></textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-
-
-
-<input name="comment[owner][name]" id="comment_owner_name" value="Joe Blog" type="text"/>
-<input name="comment[owner][email]" id="comment_owner_email" value="joe@somewhere.com" type="text"/>
-
-<input name="comment[uuid]" id="comment_uuid" value="0x" type="text"/>
-<input value="Submit" type="submit"/>
-</form>)
+<form action="/posts" method="post" accept-charset="utf-8"><input name="utf8" type="hidden" value="✓"/><input name="comment[id]" id="comment_id" value="1" type="text"/><textarea placeholder="And your story..." rows="9" name="comment[body]" id="comment_body"></textarea><input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/><input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/><input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/><input name="comment[owner][name]" id="comment_owner_name" value="Joe Blog" type="text"/><input name="comment[owner][email]" id="comment_owner_email" value="joe@somewhere.com" type="text"/><input name="comment[uuid]" id="comment_uuid" value="0x" type="text"/><input value="Submit" type="submit"/></form>)
     end
   end
 end
