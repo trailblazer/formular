5559a6b7264f53e2b4769519c25bfdd6881fa6ae
emaglio <emanuele.magliozzi@gmail.com>
2017-07-27 14:11:43 +1000

    Fixed Erb test and added some more tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'formular'
 require 'minitest/autorun'
 
-Comment = Struct.new(:id, :body, :replies, :owner, :uuid, :public, :errors) # TODO: remove errors!
+Comment = Struct.new(:id, :body, :replies, :owner, :uuid, :public, :agreed, :not_sure, :really_not_sure, :errors) # TODO: remove errors!
 Reply   = Struct.new(:id, :content, :errors)
 Owner   = Struct.new(:id, :name, :email, :errors)
 COLLECTION_ARRAY = [['Option 1', 1], ['Option 2', 2]]
