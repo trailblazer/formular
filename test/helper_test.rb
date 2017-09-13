f1ec59e1d0cc201093e6af0b3b40e56c8f91a837
Fran Worley <frances@safetytoolbox.co.uk>
2017-09-13 14:23:32 +0100

    tidy up helper tests

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -3,29 +3,33 @@ require 'formular/helper'
 require 'formular/builder'
 
 class HelperTest < Minitest::Spec
-  include Formular::Helper
+  SomeFancyBuilder = Class.new(Formular::Builders::Basic)
+  FormHelperClass = Class.new { include Formular::Helper }
 
+  form_class_instance = FormHelperClass.new
   model = OpenStruct.new
 
   it "should be Basic by default" do
     Formular::Helper._builder.must_equal :basic
+    form_class_instance.form(model, "").builder.class.must_equal Formular::Builders::Basic
   end
 
   it "should set default builder" do
     Formular::Helper.builder(:bootstrap3)
     Formular::Helper._builder.must_equal :bootstrap3
+    form_class_instance.form(model, "").builder.class.must_equal Formular::Builders::Bootstrap3
     Formular::Helper.builder(:basic) # return it back for other tests
   end
 
   it "should allow me to override the default builder" do
     Formular::Helper._builder.must_equal :basic
-    form(model, "", builder: :foundation6).builder.class.must_equal Formular::Builders::Foundation6
+    form_class_instance.form(model, "").builder.class.must_equal Formular::Builders::Basic
+    form_class_instance.form(model, "", builder: :foundation6).builder.class.must_equal Formular::Builders::Foundation6
   end
 
-  SomeClass = Class.new(Formular::Builder)
 
   it "should allow me to use my own builder" do
     Formular::Helper._builder.must_equal :basic
-    form(model, "", builder: SomeClass).builder.class.must_equal SomeClass
+    form_class_instance.form(model, "", builder: SomeFancyBuilder).builder.class.must_equal SomeFancyBuilder
   end
 end
