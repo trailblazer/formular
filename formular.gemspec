c6c887d815a3b98c6b646dd72145299f837a70f4
Nick Gorbikoff <nick.gorbikoff@gmail.com>
2018-10-04 13:32:36 -0500

    Changing HAML test and haml fixture template https://github.com/trailblazer/formular/pull/63#issuecomment-427123742

--- formular.gemspec
+++ formular.gemspec
@@ -27,6 +27,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "cells-slim"
   spec.add_development_dependency "cells-erb"
   spec.add_development_dependency "cells-haml"
+  spec.add_development_dependency "actionview"
   spec.add_development_dependency "minitest"
   spec.add_development_dependency "minitest-line"
 end
--- test/haml_test.rb
+++ test/haml_test.rb
@@ -14,9 +14,6 @@ class Comment::Haml
       render view: :show
     end
 
-    # def form(model: nil, **options, &block)
-    #   Formular::Builders::Basic.new(model: model, path_prefix: :comment).form(options, &block)
-    # end
   end
 end
 
@@ -25,91 +22,7 @@ class HamlTest < Minitest::Spec
     let(:model) { Comment.new(1, 'Nice!', [Reply.new(1, 'some exciting words'), Reply.new], Owner.new(1, 'Joe Blog', 'joe@somewhere.com'), '0x', true) }
 
     it do
-      Comment::Haml::Show.new(model).().must_equal %(<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<input name="comment[owner][name]" id="comment_owner_name" value="Joe Blog" type="text"/>
-<input name="comment[owner][email]" id="comment_owner_email" value="joe@somewhere.com" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<input name="comment[owner][name]" id="comment_owner_name" value="Joe Blog" type="text"/>
-<input name="comment[owner][email]" id="comment_owner_email" value="joe@somewhere.com" type="text"/>
-<input name="comment[uuid]" id="comment_uuid" value="0x" type="text"/>
-<input value="Submit" type="submit"/>
-<form action="/posts" method="post" accept-charset="utf-8"><input name="utf8" type="hidden" value="✓"/><div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<input name="comment[owner][name]" id="comment_owner_name" value="Joe Blog" type="text"/>
-<input name="comment[owner][email]" id="comment_owner_email" value="joe@somewhere.com" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<div>New</div>
-<input name="comment[id]" id="comment_id" value="1" type="text"/>
-<textarea name="comment[body]" id="comment_body">Nice!</textarea>
-<input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/>
-<input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/>
-<input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/>
-<input name="comment[owner][name]" id="comment_owner_name" value="Joe Blog" type="text"/>
-<input name="comment[owner][email]" id="comment_owner_email" value="joe@somewhere.com" type="text"/>
-<input name="comment[uuid]" id="comment_uuid" value="0x" type="text"/>
-<input value="Submit" type="submit"/>
-</form>)
+      Comment::Haml::Show.new(model).().must_equal %(<div>New</div>\n<form action=\"/posts\" method=\"post\" accept-charset=\"utf-8\"><input name=\"utf8\" type=\"hidden\" value=\"✓\"/><input name=\"comment[id]\" id=\"comment_id\" value=\"1\" type=\"text\"/>\n<textarea name=\"comment[body]\" id=\"comment_body\">Nice!</textarea>\n<input value=\"0\" name=\"comment[public]\" type=\"hidden\"/><input value=\"true\" name=\"comment[public]\" id=\"comment_public\" type=\"checkbox\" checked=\"checked\"/>\n<input name=\"comment[replies][][content]\" id=\"comment_replies_0_content\" value=\"some exciting words\" type=\"text\"/>\n<input name=\"comment[replies][][content]\" id=\"comment_replies_1_content\" type=\"text\"/>\n<input name=\"comment[owner][name]\" id=\"comment_owner_name\" value=\"Joe Blog\" type=\"text\"/>\n<input name=\"comment[owner][email]\" id=\"comment_owner_email\" value=\"joe@somewhere.com\" type=\"text\"/>\n<input name=\"comment[uuid]\" id=\"comment_uuid\" value=\"0x\" type=\"text\"/>\n<input value=\"Submit\" type=\"submit\"/>\n</form>)
     end
   end
 end
